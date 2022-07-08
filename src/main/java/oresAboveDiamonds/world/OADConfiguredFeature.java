5303ae2edaf0d165023f74dd73721f7b84b99e5a
Squidsword <messagemarvin@gmail.com>
2022-07-08 14:00:20 -0400

    record attempt

--- src/main/java/oresAboveDiamonds/world/OADConfiguredFeature.java
+++ src/main/java/oresAboveDiamonds/world/OADConfiguredFeature.java
@@ -8,11 +8,23 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import oresAboveDiamonds.config.OADConfig;
 import oresAboveDiamonds.init.ModBlocks;
-// Reference: Gobber by kwpugh
-public class OADConfiguredFeature {
+
+import java.util.ArrayList;
+
+public record OADPlacedFeatureRecord(String name, int veinSize, float discardChance, int timesRarer, diamondVeinCount, ImmutableList<OreConfiguration.TargetBlockState> targets, HeightRangePlacement placement, Holder<ConfiguredFeature<OreConfiguration, ?>> configured) {
+
+    private static ArrayList<OADPlacedFeatureRecord> OADConfiguredList;
+
+    public static ImmutableList<OreConfiguration.TargetBlockState> AMETHYST_TARGET_BLOCKS;
+    public static ImmutableList<OreConfiguration.TargetBlockState> NETHER_AMETHYST_TARGET_BLOCKS;
+    public static ImmutableList<OreConfiguration.TargetBlockState> END_AMETHYST_TARGET_BLOCKS;
+    public static ImmutableList<OreConfiguration.TargetBlockState> BLACK_OPAL_TARGET_BLOCKS;
+    public static ImmutableList<OreConfiguration.TargetBlockState> NETHER_BLACK_OPAL_TARGET_BLOCKS;
+    public static ImmutableList<OreConfiguration.TargetBlockState> END_BLACK_OPAL_TARGET_BLOCKS;
 
     public static Holder<ConfiguredFeature<OreConfiguration, ?>> AMETHYST_SMALL;
     public static Holder<ConfiguredFeature<OreConfiguration, ?>> BLACK_OPAL_SMALL;
@@ -25,15 +37,16 @@ public class OADConfiguredFeature {
     public static Holder<ConfiguredFeature<OreConfiguration, ?>> END_AMETHYST;
     public static Holder<ConfiguredFeature<OreConfiguration, ?>> END_BLACK_OPAL;
 
-    public static ImmutableList<OreConfiguration.TargetBlockState> AMETHYST_TARGET_BLOCKS;
-    public static ImmutableList<OreConfiguration.TargetBlockState> NETHER_AMETHYST_TARGET_BLOCKS;
-    public static ImmutableList<OreConfiguration.TargetBlockState> END_AMETHYST_TARGET_BLOCKS;
-    public static ImmutableList<OreConfiguration.TargetBlockState> BLACK_OPAL_TARGET_BLOCKS;
-    public static ImmutableList<OreConfiguration.TargetBlockState> NETHER_BLACK_OPAL_TARGET_BLOCKS;
-    public static ImmutableList<OreConfiguration.TargetBlockState> END_BLACK_OPAL_TARGET_BLOCKS;
+    public OADPlacedFeatureRecord {
+        OADConfiguredList.add(this);
+    }
 
     public static Holder<ConfiguredFeature<OreConfiguration, ?>> buildConfigured(String name, ImmutableList<OreConfiguration.TargetBlockState> targets, double veinSize, float discardChance) {
         return FeatureUtils.register(name, Feature.ORE, new OreConfiguration(targets, (int) Math.round(veinSize), discardChance));
+    }
+
+    static {
+
     }
 
     public static void registerConfiguredFeatures() {
@@ -50,6 +63,7 @@ public class OADConfiguredFeature {
 
         AMETHYST_SMALL = buildConfigured("ore_amethyst", AMETHYST_TARGET_BLOCKS, (OADConfig.amethyst_vein_size.get() / 2.0), OADConfig.overworld_discard_chance_on_air_exposure.get().floatValue() * 0.715F);
         BLACK_OPAL_SMALL = buildConfigured("ore_black_opal", BLACK_OPAL_TARGET_BLOCKS, (OADConfig.black_opal_vein_size.get() / 2.0), OADConfig.overworld_discard_chance_on_air_exposure.get().floatValue() * 0.715F);
+        new OADPlacedFeatureRecord("ore_amethyst")
 
         AMETHYST_BURIED = buildConfigured("ore_amethyst_buried", AMETHYST_TARGET_BLOCKS, OADConfig.amethyst_vein_size.get(), Math.min(1.0F, OADConfig.overworld_discard_chance_on_air_exposure.get().floatValue() * 1.43F));
         BLACK_OPAL_BURIED = buildConfigured("ore_black_opal_buried", BLACK_OPAL_TARGET_BLOCKS, OADConfig.black_opal_vein_size.get(), Math.min(1.0F, OADConfig.overworld_discard_chance_on_air_exposure.get().floatValue() * 1.43F));
@@ -69,4 +83,11 @@ public class OADConfiguredFeature {
         END_AMETHYST = buildConfigured("ore_end_amethyst", END_AMETHYST_TARGET_BLOCKS, endAmethystVeinSize, OADConfig.end_discard_chance_on_air_exposure.get().floatValue());
         END_BLACK_OPAL = buildConfigured("ore_end_black_opal", END_BLACK_OPAL_TARGET_BLOCKS, endBlackOpalVeinSize, OADConfig.end_discard_chance_on_air_exposure.get().floatValue());
     }
+
+    public ArrayList<OADPlacedFeatureRecord> getOADConfiguredList(){
+        ArrayList<OADPlacedFeatureRecord> newList = new ArrayList<>();
+        newList.addAll(OADConfiguredList);
+        return newList;
+    }
+
 }
