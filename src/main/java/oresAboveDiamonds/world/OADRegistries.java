63a9505445596c88036c4184409c2cb434ddfe1e
Squidsword <messagemarvin@gmail.com>
2023-01-02 19:51:03 -0500

    Fix compiler errors

---
+++ src/main/java/oresAboveDiamonds/world/OADRegistries.java
@@ -0,0 +1,16 @@
+package oresAboveDiamonds.world;
+
+//// Reference: Randomite 1.19.3 by Modding Legacy
+//
+//@EventBusSubscriber(modid = OresAboveDiamonds.MODID, bus = Bus.MOD)
+//public class OADDataGen {
+//    private static final RegistrySetBuilder BUILDER = new RegistrySetBuilder().add(Registries.CONFIGURED_FEATURE, (c) -> OADConfiguredFeature.bootstrap(c));
+//
+//    @SubscribeEvent
+//    public static void gatherData(GatherDataEvent event)
+//    {
+//        DataGenerator gen = event.getGenerator();
+////        gen.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(gen.getPackOutput(), BUILDER));
+//    }
+//
+//}
